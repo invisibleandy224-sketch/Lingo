49c28c10d0a675282a77785b4cf76d69c8ac3b1f
Anderson <invisibleandy224@gmail.com>
2026-05-17 11:15:28 -0400

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl text-indigo-600 text-center mt-9">Duolingo Clone.</Text>
+    </View>
+  );
+}
